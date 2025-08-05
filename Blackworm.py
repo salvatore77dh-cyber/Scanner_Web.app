42a5b3f775e8be856741092c17325ce781b348c6
Black pregnancy <salvatore77dh@gmail.com>
2025-08-05 19:22:57 -0400

    Add files via upload

---
+++ Blackworm.py
@@ -0,0 +1,171 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from colorama import Fore, Style, init
+import time
+
+init(autoreset=True)
+
+
+report = []
+
+
+def show_banner():
+    banner = Fore.CYAN + r"""
+⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘﴾ Black Worm Tool﴿⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                
+⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⠀⠀⢀⣠⣤⣶⣶⣶⣶⣶⣶⣶⣦⣤⣀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⡔⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢟⣫⣯⣷⣾⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣷⡙⢿⣿⣿⣿⣿⣿⣿⡿⢋⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⢀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣙⠿⠿⠿⢟⣫⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢹⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀  [+] Copyrit Of Salvatore                  
+⠀⠀ ⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿⣿⡏⠉⠙⢿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀ ⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣿⡿⡍⠳⣄⡀⢀⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀                                            
+⠀⠀ ⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⢿⡄⠸⡿⢄⠛⣘⢠⣼⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀                                            
+⠀⠀ ⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡞⣼⡻⡄⠳⡤⠽⠾⠿⠿⠿⢛⣻⣿⣿⣿⣷⡀⠀⠀⠀                                            
+⠀⠀ ⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣄⠙⢶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀                                            
+⠀ ⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠉⢉⣁⣀⣀⣀⣀⣀⣉⡉⠙⠛⠻⢿⣿⣿⣿⣿⣿⣯⣻⣍⡲⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀                                            
+⠀ ⢀⡀⣶⣤⣌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⣁⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣈⠛⢿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⡿⠟⠛⠛⠁⠀⠀                                            
+ ⣰⣿⣿⣿⣿⣿⣿⣿⣝⢿⣿⣿⣿⣿⣿⣿⣟⣡⣶⠿⢛⣛⣉⣭⣭⣤⣤⡴⠶⠶⠶⠶⢲⣴⣤⠭⠭⡭⣟⠻⠦⣝⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢉⣀⣠⣶⣿⣆⠀⠀                                            
+ ⠹⣿⣿⣿⣿⣿⣿⣙⠻⣿⣮⣛⠿⣿⣿⣿⣫⣵⡶⠟⣛⣋⣭⣭⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⣮⣽⣿⣿⣿⠿⠟⠛⠉⢀⣴⣿⣿⣿⣿⣿⣿⣶⡀                                            
+ ⠀⠈⠙⠋⠁⠀⠈⠉⠛⠳⣭⣛⢷⣦⣸⣿⣯⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⣿⠀⠀⠀⣀⣴⣾⣿⣿⣿⣿⡟⣿⣿⣿⣿⡇                                            
+ ⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⠿⠿⢿⣹⣿⣧⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⡏⣀⣴⣾⣿⣿⠿⠛⠉⠀⠀⠀⠈⠛⠛⠉⠀                                            
+⠀ ⠀⠀⠀⢀⣴⠿⠛⠋⠁⠀⠀⠀⢀⣯⢿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⠣⣟⡻⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀ ⠀⠀⠉⠀⠀⠀⠀⠀⢀⣀⣴⣾⣿⠈⡿⣿⠃⠀⠀⠀⠈⠉⠛⠻⠿⣿⣿⣿⣿⣿⠿⠛⠉⠉⠀⠈⠉⠛⣿⣽⡟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀ ⠀⠀⢀⣠⣤⣶⣾⣿⣿⣿⣿⣿⣀⣼⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⣹⡟⣻⣿⡃⠀⠀⠀⠀⠀⠀⠀⠀⢹⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀ ⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⡿⢹⣿⣿⣿⡄⠀⠀⠀⠀+⠀⠀⣰⣿⢣⡇⣿⣷⡀⠀⠀+⠀⠀⠀⠀⣼⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀ ⠟⠋⠙⠛⠻⣿⣿⣿⣿⣿⠏⠀⠈⢿⣿⣿⣿⣦⣄⣀⣀⣀⣠⣴⣿⣏⡞⢻⣸⣿⣷⣄⠀⠀⣀⣤⠴⣾⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀ ⠀⠀⠀⠀⠀⠹⣿⣿⠃⠀⠀⠀⠈⢿⣿⣵⣾⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣶⠾⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠘⣏⠀⠀⠀⠀⠀⣀⣼⣿⡛⢿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⠟⣡⣾⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠘⠀⠀⢠⣶⣿⣿⣯⣿⡇⠀⢹⣿⣿⣿⣿⣷⣤⣤⣦⣶⣿⣿⣿⣿⣿⡇⠀⣿⣿⢸⣿⣶⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀⠀⠀ ⠀⠀⢀⣄⣠⣴⣾⣿⣟⣿⠟⠁⣿⡇⠀⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⡇⢀⣿⣿⠙⢮⣛⠿⣷⣦⣄⣀⣀⣀⣠⣀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀ ⠀⣴⣶⣶⣾⣿⣿⡿⣛⣽⠞⠋⠀⠀⠀⣿⣷⠀⣍⠇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠉⡄⣸⣿⡿⠀⠀⠈⠙⠮⣟⠿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀ ⢀⣹⣿⣿⣿⢵⡿⠋⠀⠀⠀⠀⠀⠀⢿⣿⣦⣿⡷⣄⠙⠿⣿⢹⣿⣿⢼⡿⠋⣡⣶⣳⣿⣿⣿⠃⠀⠀⠀⠀⠀⠈⠿⠬⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀ ⠸⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣷⣻⢿⣶⣬⣈⣉⣉⣤⣴⣿⣻⣾⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⡿⠇⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀⠀ ⠀⠙⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⡇⣿⣇⣿⢹⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                            
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                                                     
+⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘﴾ Scanner Web app  ﴿⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘
+"""
+    print(banner)
+    report.append("============= Black worm Scanner - Vulenrabillity Report==============\n")
+
+
+def scan_xss(url):
+    print(Fore.MAGENTA + "\n[+] Scanner for Xss ......")
+    XSS_PAYLOAD = "<script>alert('XSS')</script>"
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    forms = soup.find_all("form")
+    print(Fore.YELLOW + f"[*] Found {len(forms)} forms on {url}")
+    report.append(f"[-] Found {len(forms)} forms on {url}")
+    for form in forms:
+        details = {
+            "action": form.attrs.get("action"),
+            "method": form.attrs.get("method", "get").lower(),
+            "input": []
+        }
+        for input_tag in form.find_all("input"):
+            name = input_tag.attrs.get("name")
+            input_type = input_tag.attrs.get("type", "text")
+            if name:
+                details["inputs"].append({"name": name, "type": input_type})
+        target_url = urljoin(url, details["action"])
+        data = {}
+        for input in details["inputs"]:
+            if input["type"] == "text":
+                data[input["namz"]] = XSS_PAYLOAD
+            else:
+                data[input["name"]] = "test"
+        if details["method"] == "post":
+            res = requests.post(target_url, data=data)
+        else:
+            res = requests.get(target_url, params=data)
+        if XSS_PAYLOAD in res.text:
+            print(Fore.RED + "[!] xss vulnerabillity found!")
+            print(Fore.RED + f" -> Form action: {details['action']}")
+            report.append("[!] Xss vulerabllity found!")
+            report.append(f" -> Form action: {details['action']}")
+        else:
+            print(Fore.GREEN + "[+] No xss detected.")
+            report.append("[+] No xss detected.")
+
+
+def scan_sql_injection(url):
+    print(Fore.MAGENTA + "\n[*] Scanning or SQL injection....")
+    SQL_PAYLOD = "' OR '1'='1"
+    test_url = f"{url}?id={SQL_PAYLOD}"
+    res = requests.get(test_url)
+    errors = ["you have an error", "warning", "mysql", "syntax error"]
+    if any (e in res.txt.lower() for e in errors):
+        print(Fore.RED + f"[!] SQL injection found at: {test_url}")
+        report.append(f"[!] SQL Injection found at: {test_url}")
+    else:
+        print(Fore.GREEN + "[+] No SQL injection detected.")
+        report.append("[+] No SQL Injection detected.")
+
+
+def scan_open_redirect(url):
+    print(Fore.MAGENTA + "\n[*] Scaning for Openf Redirect....")
+    REDIRECT_PAYLOAD = + "https://voluble-peony-8f34fb.nettlify.app"
+    test_url = f"{url}?redirect={REDIRECT_PAYLOAD}"
+    res = requests.get(test_url, allow_redirects=False)
+    if res.status_code in [301, 302] and REDIRECT_PAYLOAD in res.headers.get("Locations", ""):
+        print(Fore.RED + f"[!] Open Redirect Found at: {test_url}")
+        report.append(f"[!] Open Redirect Found at: {test_url}")
+    else:
+        print(Fore.GREEN + "[+] No Open Redirect detected.")
+        report.append("[+] No Open Redirect detected.")
+
+
+def check_security_headers(url):
+    print(Fore.MAGENTA + "\n[*] Checking Security Headers...")
+    res = requests.get(url)
+    headers = res.headers
+    expected = [
+        'X-Frame-Options',
+        'X-XSS-Protection',
+        'Strict-Transport-Security',
+        'Content-Security-Policy'
+    ]
+    for h in expected:
+        if h in headers:
+            print(Fore.GREEN + f"[+] {h} is set.")
+            report.append(f"[+] {h} is set.")
+        else:
+            print(Fore.RED + f"[!] {h} is missing!")
+            report.append(f"[!] {h} is missing!")
+
+
+def save_report():
+    with open("report.txt", "w", encoding="utf-8") as f:
+        for line in report:
+            f.write(line + "\n")
+    print(Fore.CYAN + "\n# Report saved to: report.txt")
+
+    html = "<html><head><title> BlackWorm Report</title></head><body><ht>Vulenrabillity Report</h1><ul>"
+    for line in report:
+        color = "green" if "[+]" in line else "red" if "[!]" in line else "black"
+        html += f"<li style='color:{color}'>{line}</li>"
+    html += "</ul></body></html>"
+    with open("report.html", "w", encoding="utf-8") as f:
+        f.write(html)
+    print(Fore.CYAN + "# HTML report Saved to: report.html")
+
+
+def main():
+    show_banner()
+    url = input(Fore.YELLOW + " Enter URL to scan (e.g: https://example.com)").strip()
+    time.sleep(1)
+
+    try:
+        scan_xss(url)
+        scan_sql_injection(url)
+        scan_open_redirect(url)
+        check_security_headers(url)
+    except Exception as e:
+        print(Fore.RED + f"[x] Error: {e}")
+
+    save_report()
+
+if __name__ == "__main__":
+    main()
